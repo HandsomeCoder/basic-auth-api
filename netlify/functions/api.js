bce3ba943dc0c566b6f23fbff65d60fae5f4201d
Harsh Shah <harshshah1295@gmail.com>
2023-06-26 02:06:37 -0400

    Setup express

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -5,14 +5,15 @@ const logger = require('morgan');
 const cors = require('cors')
 const compression = require('compression');
 
+const router = require('./routes');
+router.get("/", (_, res) => res.json({ status: true }));
+
 const api = express();
+
 api.use(express.json({limit: '1mb'}));
 api.use(cors())
 api.use(logger('dev'));
 api.use(compression());
-
-const router = require('./routes');
-router.get("/", (_, res) => res.json({ status: true }));
 api.use("/.netlify/functions/api", router);
 
 export const handler = serverless(api);
