8dc258875b8c6c932f83ac9761f582814b706530
Harsh Shah <harshshah1295@gmail.com>
2023-06-26 01:54:31 -0400

    Fix routes

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -4,7 +4,7 @@ import serverless from "serverless-http";
 const api = express();
 
 const router = require('./routes');
-router.get("", (_, res) => res.json({ status: true }));
-api.use("/.netlify/functions/api/", router);
+router.get("/", (_, res) => res.json({ status: true }));
+api.use("/.netlify/functions/api", router);
 
 export const handler = serverless(api);
