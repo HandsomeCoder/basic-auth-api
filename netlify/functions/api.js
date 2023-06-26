62be313a351b37c18aba1bd58b6981794633fc61
Harsh Shah <harshshah1295@gmail.com>
2023-06-26 02:03:18 -0400

    Setup express

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,7 +1,17 @@
-import express from "express";
+const express = require('express');
+const serverless = require("serverless-http");
+
 import serverless from "serverless-http";
 
+const logger = require('morgan');
+const cors = require('cors')
+const compression = require('compression');
+
 const api = express();
+app.use(express.json({limit: '1mb'}));
+api.use(cors())
+api.use(logger('dev'));
+api.use(compression());
 
 const router = require('./routes');
 router.get("/", (_, res) => res.json({ status: true }));
